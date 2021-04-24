277ecda92a53f12a64875813ac29644c28f7f2fa
cbun1123 <83135128+cbun1123@users.noreply.github.com>
2021-04-24 19:55:32 +0200

    Update frenzy-clicker.js

--- frenzy-clicker.js
+++ frenzy-clicker.js
@@ -1,13 +1,21 @@
-var clickTimer = 1000/250;
+var clickTimer = 1000/25;
 var checkTimer = 1000;
 var checkFunc = function() {
-  if (Game.hasBuff('Click frenzy')) {
+  if (Game.hasBuff("Click frenzy") ||
+      Game.hasBuff("Cursed finger") ||
+      Game.hasBuff("Devastation") ||
+      Game.hasBuff("Dragonflight") ||
+      Game.hasBuff("Frenzy")) {
     clearInterval(timerId);
     timerId = setInterval(clickFunc, clickTimer);
   }
 };
 var clickFunc = function() {
-  if (Game.hasBuff('Click frenzy')) {
+  if (Game.hasBuff("Click frenzy") ||
+      Game.hasBuff("Cursed finger") ||
+      Game.hasBuff("Devastation") ||
+      Game.hasBuff("Dragonflight") ||
+      Game.hasBuff("Frenzy")) {
     Game.ClickCookie();
   } else {
     clearInterval(timerId);
